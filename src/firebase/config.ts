136f647b932801dee6843d248d24f4fa21b61ce8
Tech World <techworldinfo98@gmail.com>
2025-10-01 16:29:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2336669417-6f101",
+  "appId": "1:115893744059:web:99e27b88c35c019187949e",
+  "apiKey": "AIzaSyCcbanPdyKc04u4zuR8WvOAn-o37XoSUfc",
+  "authDomain": "studio-2336669417-6f101.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "115893744059"
+};
